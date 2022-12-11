f20352e8d3b78bc1d35ff37afdae3713df7d8920
irvinzato <multizato@hotmail.com>
2022-12-11 01:19:33 -0600

    Construi clase con programacion reactiva y empece a utilizarla en controlador

--- src/main/java/com/rivera/ControllerCustomer.java
+++ src/main/java/com/rivera/ControllerCustomer.java
@@ -1,7 +1,10 @@
 package com.rivera;
 
+import com.rivera.comunicationotherservices.CommunicationServiceProduct;
 import com.rivera.entities.Customer;
 import com.rivera.repositories.RepositoryCustomer;
+import io.smallrye.common.annotation.Blocking;
+import io.smallrye.mutiny.Uni;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
@@ -16,12 +19,17 @@ public class ControllerCustomer {
     @Inject
     private RepositoryCustomer repositoryCustomer;
 
+    @Inject
+    private CommunicationServiceProduct communicationServiceProduct;
+
     @GET
+    @Blocking //Para evitar error, ya que es reactivo pero la base de datos no lo es, por lo tanto es bloqueante
     public List<Customer> getCustomers() {
         return repositoryCustomer.listCustomer();
     }
 
     @POST
+    @Blocking //Para evitar error, ya que es reactivo pero la base de datos no lo es, por lo tanto es bloqueante
     public Response createCustomer(Customer customer){
         repositoryCustomer.createCustomer(customer);
         return Response.accepted().build();
@@ -29,6 +37,7 @@ public class ControllerCustomer {
 
     @GET
     @Path("/{id}")
+    @Blocking //Para evitar error, ya que es reactivo pero la base de datos no lo es, por lo tanto es bloqueante
     public Response findById(@PathParam("id") Long id){
         Customer customer = repositoryCustomer.byId(id);
         return Response.ok(customer).build();
@@ -36,13 +45,15 @@ public class ControllerCustomer {
 
     @GET
     @Path("/{id}/product")
-    public Response findByIdProducts(@PathParam("id") Long id){
-        //En construcción funcionamiento
-        return Response.ok().build();
+    @Blocking //Para evitar error, ya que es reactivo pero la base de datos no lo es, por lo tanto es bloqueante
+    public Uni<Customer> findByIdProducts(@PathParam("id") Long id){
+        //¡Llamado a clase donde utilizo programación reactiva!
+        return communicationServiceProduct.compareCustomerWhitProducts(id);
     }
 
     @DELETE
     @Path("/borrar/{id}")
+    @Blocking //Para evitar error, ya que es reactivo pero la base de datos no lo es, por lo tanto es bloqueante
     public Response deleteCustomer(@PathParam("id") Long id) {
         repositoryCustomer.deleteCustomer(id);
         return Response.ok().build();
@@ -50,6 +61,7 @@ public class ControllerCustomer {
 
     @PUT
     @Path("/editar/{id}")
+    @Blocking //Para evitar error, ya que es reactivo pero la base de datos no lo es, por lo tanto es bloqueante
     public Response editCustomer(@PathParam("id") Long id, Customer customer) {
         Customer customerUpdate = repositoryCustomer.editCustomer(id, customer);
         return Response.ok(customerUpdate).build();
--- src/main/java/com/rivera/comunicationotherservices/CommunicationServiceProduct.java
+++ src/main/java/com/rivera/comunicationotherservices/CommunicationServiceProduct.java
@@ -1,10 +1,13 @@
 package com.rivera.comunicationotherservices;
 
+import javax.annotation.PostConstruct;
+import javax.inject.Singleton;
+import java.util.ArrayList;
+import javax.inject.Inject;
+import java.util.List;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.rivera.entities.Customer;
-import com.rivera.entities.Product;
-import com.rivera.repositories.RepositoryCustomer;
+import io.smallrye.common.annotation.Blocking;
 import io.smallrye.mutiny.Uni;
 import io.vertx.core.json.JsonArray;
 import io.vertx.ext.web.client.WebClientOptions;
@@ -12,13 +15,12 @@ import io.vertx.mutiny.core.Vertx;
 import io.vertx.mutiny.ext.web.client.WebClient;
 import lombok.extern.slf4j.Slf4j;
 
-import javax.annotation.PostConstruct;
-import javax.inject.Inject;
-import javax.inject.Singleton;
-import java.util.ArrayList;
-import java.util.List;
+import com.rivera.entities.Customer;
+import com.rivera.entities.Product;
+import com.rivera.repositories.RepositoryCustomer;
 
 //Esta clase utiliza librerías del POM, "quarkus-vertx" y "smallrye-mutiny-vertx-web-client"
+//Para programación reactiva
 @Slf4j
 @Singleton
 public class CommunicationServiceProduct {
@@ -31,7 +33,7 @@ public class CommunicationServiceProduct {
 
   private WebClient webClient;
 
-  //Metodo que se inicializa después de haber construido la clase
+  //Metodo que se inicializa después de haber construido la clase, utilizado para consumir otra ruta
   @PostConstruct
   void initialize(){
     this.webClient = WebClient.create(vertx,
@@ -68,7 +70,21 @@ public class CommunicationServiceProduct {
               });
               return listProducts;
             });
+  }
 
+  public Uni<Customer> compareCustomerWhitProducts(Long id){
+    return Uni.combine().all().unis(getCustomerReactive(id), getAllProduct())
+            .combinedWith((customer, products) -> {
+              customer.getProducts().forEach(prod -> {
+                products.forEach(p -> {
+                  if(prod.getProduct().equals(p.getId())) {
+                    prod.setName(p.getName());
+                    prod.setDescription(p.getDescription());
+                  }
+                });
+              });
+              return customer;
+            });
   }
 
 }
